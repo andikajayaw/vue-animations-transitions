80a13ff4d8c6f5013f6d284f84f6d60a59a94de1
andikajayaw <andikajayawiguna@gmail.com>
2021-02-03 22:39:08 +0800

    animating a modal

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible..</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -93,35 +93,18 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
+.para-enter-active {
+  /* transition: all 0.3s ease-out; */
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-active {
-  transition: all 0.3s ease-out;
+.para-leave-active {
+  /* transition: all 0.3s ease-in; */
+  animation: slide-scale 0.3s ease-out;
 }
 
-.v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
-}
 
-.v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
-}
-
-.v-leave-active {
-  transition: all 0.3s ease-in;
-}
-
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
-}
-
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
