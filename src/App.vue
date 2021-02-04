17418103997ca869ef64dda427e4a82dc24b1e60
andikajayaw <andikajayawiguna@gmail.com>
2021-02-04 22:46:52 +0800

    animation route changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="container">
+    <router-view v-slot="slotProps">
+      <transition name="fade-button" mode="out-in">
+        <component :is="slotProps.Component"></component>
+      </transition>
+    </router-view>
+  <!-- <div class="container">
     <UserList />
   </div>
   <div class="container">
@@ -34,15 +39,15 @@
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
-  </div>
+  </div> -->
 </template>  
 
 <script>
-import UserList from './components/ListData.vue';
+// import UserList from './components/ListData.vue';
 export default {
-  components: {
-    UserList
-  },
+  // components: {
+  //   // UserList
+  // },
   data() {
     return { 
       animatedBlock: false,
@@ -130,82 +135,96 @@ export default {
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-}
-html {
-  font-family: sans-serif;
-}
-body {
-  margin: 0;
-}
-button {
-  font: inherit;
-  padding: 0.5rem 2rem;
-  border: 1px solid #810032;
-  border-radius: 30px;
-  background-color: #810032;
-  color: white;
-  cursor: pointer;
-}
-button:hover,
-button:active {
-  background-color: #a80b48;
-  border-color: #a80b48;
-}
-.block {
-  width: 8rem;
-  height: 8rem;
-  background-color: #290033;
-  margin-bottom: 2rem;
-  /* transition: transform 0.3s ease-out; */
-}
-.container {
-  max-width: 40rem;
-  margin: 2rem auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 2rem;
-  border: 2px solid #ccc;
-  border-radius: 12px;
-}
-
-.animate {
-  /* transform: translateX(-150px); */
-  animation: slide-fade 2s ease-out forwards;
-}
-
-.fade-button-enter-from,
-.fade-button-leave-to {
-  opacity: 0;
-}
-
-.fade-button-enter-active {
-  transition: opacity 0.3s ease-out;
-}
-
-.fade-button-leave-active {
-  transition: opacity 0.3s ease-in;
-}
-
-.fade-button-enter-to,
-.fade-button-enter-from {
-  opacity: 1;
-}
-
-@keyframes slide-scale {
-  0% {
-    transform: translateX(0) scale(1);
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    font-family: sans-serif;
+  }
+  body {
+    margin: 0;
+  }
+  button {
+    font: inherit;
+    padding: 0.5rem 2rem;
+    border: 1px solid #810032;
+    border-radius: 30px;
+    background-color: #810032;
+    color: white;
+    cursor: pointer;
+  }
+  button:hover,
+  button:active {
+    background-color: #a80b48;
+    border-color: #a80b48;
+  }
+  .block {
+    width: 8rem;
+    height: 8rem;
+    background-color: #290033;
+    margin-bottom: 2rem;
+    /* transition: transform 0.3s ease-out; */
+  }
+  .container {
+    max-width: 40rem;
+    margin: 2rem auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 2rem;
+    border: 2px solid #ccc;
+    border-radius: 12px;
   }
 
-  70% {
-    transform: translateX(-120px) scale(1.1);
+  .animate {
+    /* transform: translateX(-150px); */
+    animation: slide-fade 2s ease-out forwards;
   }
 
-  100% {
-    transform: translateX(-150px) scale(1);
+  .fade-button-enter-from,
+  .fade-button-leave-to {
+    opacity: 0;
   }
-}
+
+  .fade-button-enter-active {
+    transition: opacity 0.3s ease-out;
+  }
+
+  .fade-button-leave-active {
+    transition: opacity 0.3s ease-in;
+  }
+
+  .fade-button-enter-to,
+  .fade-button-enter-from {
+    opacity: 1;
+  }
+
+  .router-enter-from {
+
+  }
+  .router-enter-active {
+    animation: slide-scale 1s ease-out;
+  }
+  .router-enter-to {
+    
+  }
+
+  .router-leave-active {
+    animation: slide-scale 1s ease-in;
+  }
+
+  @keyframes slide-scale {
+    0% {
+      transform: translateX(0) scale(1);
+    }
+
+    70% {
+      transform: translateX(-120px) scale(1.1);
+    }
+
+    100% {
+      transform: translateX(-150px) scale(1);
+    }
+  }
 </style>
